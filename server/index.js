185a7191f1b00774bd9aecb655bce76088de2672
subhammishr <msubham193@gmail.com>
2023-10-10 01:23:32 +0530

    intial commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const typeDefs = require("./graphql/typeDefs");
+
+const gql = require("graphql-tag");
+
+const Post = require("./models/Post");
+const User = require("./models/User");
+
+const resolvers = {
+  Query: {},
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Database connection established");
+    server.listen(process.env.PORT || 4000, () => {
+      console.log(
+        `server is listening on http://localhost:${process.env.PORT}`
+      );
+    });
+  })
+  .catch((error) => console.log(error));
